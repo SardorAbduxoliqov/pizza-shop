c789eda3626563c3401f13c2603eb3fc40fa2cc1
SardorAbduxoliqov <sardornewgithub@mail.ru>
2022-01-22 10:51:08 +0500

    first commit

---
+++ components/PizzaList.jsx
@@ -0,0 +1,24 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus ab
+        explicabo exercitationem et nesciunt ex, in enim doloribus veritatis
+        minima excepturi animi odit quos dignissimos, error adipisci nam aperiam
+        cum.
+      </p>
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
