1a09fb60938594c3e371d17ba12ee147a89e6a6f
SardorAbduxoliqov <sardornewgithub@mail.ru>
2022-02-05 11:49:51 +0500

    admin added

---
+++ pages/admin/index.jsx
@@ -0,0 +1,46 @@
+import styles from "../../styles/Admin.module.css";
+import Image from "next/image";
+
+const index = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Products</h1>
+        <table className={styles.table}>
+          <tbody>
+            <tr className={styles.trTitle}>
+              <th>Image</th>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr className={styles.trTitle}>
+              <td>
+                <Image
+                  src="/img/pizza.png"
+                  width={50}
+                  height={50}
+                  objectFit="cover"
+                  alt=""
+                />
+              </td>
+              <td>PizzaId</td>
+              <td>Pizza Title</td>
+              <td>$50</td>
+              <td>
+                <button className={styles.button}>Edit</button>
+                <button className={styles.button}>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.item}></div>
+    </div>
+  );
+};
+
+export default index;
